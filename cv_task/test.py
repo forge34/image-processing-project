8bd3c989cf2408812ce73363bb7b1ac0a3374c26
Mohamed Abdeltawab <mohamedabdeltawab338@gmail.com>
2026-05-08 02:24:40 +0300

    feat: add yunet model

--- cv_task/test.py
+++ cv_task/test.py
@@ -1,9 +1,11 @@
 import cv2
-from model import detect_face
+from model_haar import detect_face
+from model_yunet import detect_face_yunet
 
 expected_faces = 2
 
-img = cv2.imread("./assets/images.jpeg")
-result, n, t = detect_face(img)
+img = cv2.imread("./assets/test1.jpeg")
+result, n, t = detect_face_yunet(img)
+cv2.imwrite("result.png",result)
 print("Time taken: ", t)
-print("Success rate: ", n == expected_faces, "%")
+print("Success ", n == expected_faces)
